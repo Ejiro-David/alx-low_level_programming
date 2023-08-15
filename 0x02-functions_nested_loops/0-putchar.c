805e39b0a6b3264c71f3fe2cd616ff4773e9dc04
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-08-15 14:58:55 -0700

    Put Charx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: success (0), ALways
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+loops within loops within loops
