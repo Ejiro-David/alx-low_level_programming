21a3e3295fdb8f74786f92177b7192184c3952ed
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-08-16 15:55:00 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Void.
+ */
+
+
+void main(int n)
+{
+int i, sum;
+for (i = 1023; i > 0; i--)
+{
+if(i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+else
+{
+sum;
+}
+}
+printf("%d\n", sum);
+}
