fb5b17ddda1ca42e09c14cb4e4ff7f084ccb98a5
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-09-05 21:56:03 -0700

    almost there

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,18 @@
 
 char *create_array(unsigned int size, char c)
 {
-char *ar;
+	char *array;
+	unsigned int i;
 
-if (size != 0)
-{
-	ar = malloc(size * sizeof(char));
-        ar[0] = c;	
-}
-else
-{
-	return (NULL);
-}
-return (ar);
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
 }
