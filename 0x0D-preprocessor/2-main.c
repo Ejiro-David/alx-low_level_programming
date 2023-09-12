527e15de1114dafc6cff2228257c31faa238e3c1
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-09-11 18:11:55 -0700

    Task 1 - 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints out the name of the file it was conpiled from
+ */
+
+int main(void)
+{
+	printf("FILE: %s\n", __FILE__);
+	return EXIT_SUCCESS;
+}
