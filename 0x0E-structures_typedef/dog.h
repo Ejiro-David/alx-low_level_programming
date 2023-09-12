d380ebab1bf0c0ee4a53f95e55bcc4b0f32c556c
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-09-11 21:48:05 -0700

    minor corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,32 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog -  defines dog
+ * @name: name of dog
+ * @age: age
+ * @owner: who get dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
-typedef struct dog_t {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog_t -  defines dog
+ * @name: name of dog
+ * @age: age
+ * @owner: who get dog
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
