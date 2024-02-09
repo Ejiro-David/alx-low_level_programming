757740d48097cec52f24756a9df1c90cfe219a36
Ejiro-David <daviderhabor.de@gmail.com>
2024-02-08 18:15:57 -0800

    added all needed lib, fixed unsed var

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * binary_to_unit - converts binary to whole number base ten
@@ -11,13 +13,13 @@ unsigned int binary_to_uint(const char *b)
 unsigned int ans = 0;
 int i = 0;
 
-if (b = NULL)
+if (b == NULL)
 return 0;
 while (b[i] == '0' || b[i] == '1')
 {
-val <<= 1;
-val += b[i]-'0';
+ans <<= 1;
+ans += b[i]-'0';
 i++;
 }	
-return val;
+return ans;
 }
