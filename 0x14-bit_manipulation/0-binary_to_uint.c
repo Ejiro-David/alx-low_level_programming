2d9ac09b8a38f8e1017dd58d2d14c527a6c22da2
Ejiro-David <daviderhabor.de@gmail.com>
2024-02-08 18:20:06 -0800

    added all needed lib, fixed unsed var

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,12 @@ unsigned int ans = 0;
 int i = 0;
 
 if (b == NULL)
-return 0;
+return (0);
 while (b[i] == '0' || b[i] == '1')
 {
 ans <<= 1;
-ans += b[i]-'0';
+ans += b[i] - '0';
 i++;
-}	
-return ans;
+}
+return (ans);
 }
