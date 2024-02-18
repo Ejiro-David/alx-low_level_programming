3632ed4d07ea75d835ac1b193e79fe303e3e1f11
Ejiro-David <daviderhabor.de@gmail.com>
2024-02-18 10:42:54 -0800

    add: decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,23 +2,25 @@
 #include <stddef.h>
 #include <stdio.h>
 
+/**
+ * print_binary - converts decimal to binary
+ * @n: decimal to be conv
+ */
+
 void print_binary(unsigned long int n)
 {
-	unsigned int bitmask = 1 << 31;
-	int i = 0;
-	int leadingZeros = 1;
- 
-	for(i = 0; i < 32; i++)
-	{
-		int bit = (n & bitmask) != 0;
+unsigned int bitmask = 1 << 31;
+int i = 0;
+int leadingZeros = 1;
 
-		if(bit || !leadingZeros){
-			printf("%d", bit);
-			leadingZeros = 0;
-		}
-
-		bitmask >>= 1;
-		
-	}
-
+for (i = 0; i < 32; i++)
+{
+int bit = (n & bitmask) != 0;
+if (bit || !leadingZeros)
+{
+printf("%d", bit);
+leadingZeros = 0;
+}
+bitmask >>= 1;
+}
 }
