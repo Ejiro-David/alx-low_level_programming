53b9c805ca88636be088cbac87281bf5324de807
Ejiro-David <daviderhabor.de@gmail.com>
2024-02-24 00:55:00 -0800

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,11 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int clear_bit(unsigned long int *n, unsigned int index) {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1;
-    }
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
 
-    *n = *n & (0UL << index);
-    return 1;
+*n = *n & (0UL << index);
+return (1);
 }
