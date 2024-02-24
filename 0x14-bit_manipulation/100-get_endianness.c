d7e5ad0a0ff01561a4f8b4cac3370b746f7f5d1a
Ejiro-David <daviderhabor.de@gmail.com>
2024-02-24 01:38:27 -0800

    Team Little Endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - check your hardware's Endianess
+ * Return: 0 if big endian or 1 if little Endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *bytePointer = (char *) & i;
+
+	if (*bytePointer == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
